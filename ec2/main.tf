dd4609cb36c2aa000a95e2f71f7e1b81f0c3f076
Rohan Gupta <rohan.gupta@harness.io>
2019-08-26 13:02:23 -0700

    ec2/main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "ubuntu" {
     ami = "${var.aws_region}"
     instance_type = "${var.instance_type}"
-    availability_zone = "${var.aws_region}"
+    #availability_zone = "${var.aws_region}"
 
 
 
