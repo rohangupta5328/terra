bc42f0c78fdc93aaaa1dd512884af6035147251b
Rohan Gupta <rohan.gupta@harness.io>
2019-08-27 12:01:44 -0700

    experimenting

--- lambda/main.tf
+++ lambda/main.tf
@@ -14,24 +14,24 @@ data "archive_file" "zip" {
 }
 
 
-data "aws_iam_policy_document" "policy" {
-  statement {
-    sid = ""
-    effect = "Allow"
+# data "aws_iam_policy_document" "policy" {
+#   statement {
+#     sid = ""
+#     effect = "Allow"
 
-    principals {
-      identifiers = ["lambda.amazonaws.com"]
-      type = "Service"
-    }
+#     principals {
+#       identifiers = ["lambda.amazonaws.com"]
+#       type = "Service"
+#     }
 
-    actions = ["sts:AssumeRole"]
-  }
-}
+#     actions = ["sts:AssumeRole"]
+#   }
+# }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
-  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
-}
+# resource "aws_iam_role" "iam_for_lambda" {
+#   name = "iam_for_lambda"
+#   assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+# }
 
 resource "aws_lambda_function" "lambda" {
   function_name = "hello_lambda"
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "lambda" {
   filename = "${data.archive_file.zip.output_path}"
   source_code_hash = "${data.archive_file.zip.output_base64sha256}"
 
-  role = "${aws_iam_role.iam_for_lambda.arn}"
+  #role = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
   runtime = "python3.6"
 
