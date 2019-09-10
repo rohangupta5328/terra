6a9bc0317811894ddcfea6a88291440c06eb95cc
Rohan Gupta <rohan.gupta@harness.io>
2019-09-09 17:38:42 -0700

    ecs sample

---
+++ ecs/main.tf
@@ -0,0 +1,38 @@
+resource "aws_cloudwatch_log_group" "hello_world" {
+  name              = "hello_world"
+  retention_in_days = 1
+}
+
+resource "aws_ecs_task_definition" "hello_world" {
+  family = "hello_world"
+
+  container_definitions = <<EOF
+[
+  {
+    "name": "harness_bb_ecs_demo",
+    "image": "hello-world",
+    "cpu": 0,
+    "memory": 128,
+    "logConfiguration": {
+      "logDriver": "awslogs",
+      "options": {
+        "awslogs-region": "us-east-1",
+        "awslogs-group": "harness_bb_ecs_demo",
+        "awslogs-stream-prefix": "complete-ecs"
+      }
+    }
+  }
+]
+EOF
+}
+
+resource "aws_ecs_service" "hello_world" {
+  name = "hello_world"
+  cluster = "${var.cluster_id}"
+  task_definition = "$aws_ecs_task_definition.hello_world.arn"
+
+  desired_count = 1
+
+  deployment_maximum_percent = 100
+  deployment_minimum_healthy_percent = 0
+}
