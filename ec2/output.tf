a4f051ad4c4648ed12ec7dd268a1a4f915439b49
Rohan Gupta <rohan.gupta@harness.io>
2019-09-25 09:55:41 -0700

    removing region

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,6 +1,6 @@
-output "region" {
-    value = "${aws_instance.jenkins-server.region}"
-}
+# output "region" {
+#     value = "${aws_instance.jenkins-server.region}"
+# }
 
 output "ami" {
     value = "${aws_instance.jenkins-server.ami}"
