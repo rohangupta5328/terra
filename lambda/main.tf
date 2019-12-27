01b5ba5e6f469cfef33cf9e47a20a292cea2fd28
Rohan Gupta <52221549+rohangupta5328@users.noreply.github.com>
2019-12-27 18:28:25 -0500

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "ec2-remote-state"
-    key    = "my-app-prod/terraform.tfstate"
+    key    = "my-app-dev/terraform.tfstate"
     region = "us-east-1"
   }
     
