be5bdb7fb772562e37e0159480915595e6e99f53
Rohan Gupta <52221549+rohangupta5328@users.noreply.github.com>
2019-12-27 18:21:38 -0500

    Update main.tf

--- lambda/main.tf
+++ lambda/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "${var.aws_region}"
-    access = "${var.aws_access_key}"
-    secret = "${var.aws_secret_key}"
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
   
 }
 
