4e878545d65938510d5a1b6558417efdc04fb842
Rohan Gupta <52221549+rohangupta5328@users.noreply.github.com>
2020-05-12 19:24:21 -0700

    Update output.tf

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,7 +1,11 @@
 output "region" {
-    value = "${aws_instance.jenkins-server.region}"
+    value = "${aws_instance.ec2.region}"
 }
 
 output "ami" {
-    value = "${aws_instance.jenkins-server.ami}"
+    value = "${aws_instance.ec2.ami}"
+}
+
+output "vpc" {
+    value = "${aws_instance.ec2.vpc}"
 }
