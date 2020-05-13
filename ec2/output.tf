70cde3f792b17de407e61b0fa888e1653d4687b7
Rohan Gupta <52221549+rohangupta5328@users.noreply.github.com>
2020-05-12 22:40:15 -0700

    Update output.tf

--- ec2/output.tf
+++ ec2/output.tf
@@ -1,11 +1,11 @@
-output "region" {
-    value = "${aws_instance.ec2.region}"
-}
+# output "region" {
+#     value = "${aws_instance.ec2.region}"
+# }
 
-output "ami" {
-    value = "${aws_instance.ec2.ami}"
-}
+# output "ami" {
+#     value = "${aws_instance.ec2.ami}"
+# }
 
-output "vpc" {
-    value = "${aws_instance.ec2.vpc}"
-}
+# output "vpc" {
+#     value = "${aws_instance.ec2.vpc}"
+# }
